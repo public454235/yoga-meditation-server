4bddd347d6e412c75faf0e09abb9ee9a0eb8c779
md:robin mia <mdrobinmiaro1@gmail.com>
2023-06-14 18:12:03 +0600

    student done

--- index.js
+++ index.js
@@ -69,11 +69,31 @@ async function run() {
 
         // users api
 
-        app.get('/users', async (req, res) => {
-            const result = await usersCollection.find().toArray()
+        // student
+        app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            if (req.decoded.email !== email) {
+                return send({ instructor: false })
+            }
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            const result = { instructor: user?.role == "instructor" }
             res.send(result)
         })
 
+        app.patch('/users/instructor/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'instructor'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
+        // save user email and role in mongodb
         app.put('/users/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body
@@ -85,6 +105,7 @@ async function run() {
             const result = await usersCollection.updateOne(query, updateDoc, options)
             res.send(result)
         })
+
 
 
         // class api
